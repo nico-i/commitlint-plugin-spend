1cf2762d3cbcd0bcda3e0a5d4075ebea62d0535d
Nico Ismaili <nico.ismaili@aoe.com>
2025-06-01 23:19:19 +0200

    chore: added commitlint plugin to commitlint config
    /spend 5m

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
+import type { Plugin } from "@commitlint/types";
 import { validateCommitMsg } from "./src/utils/validateCommitMsg/validateCommitMsg";
 
 export default {
   rules: {
     spend: validateCommitMsg,
   },
-};
+} satisfies Plugin;
